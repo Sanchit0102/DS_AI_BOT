220a0ff73cb9fedf928a2b76e950b9dbba343d55
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-10-30 17:39:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,18 +4,18 @@ from os import environ
 
 id_pattern = re.compile(r'^.\d+$')
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-PICS = os.environ.get("PICS", "").split()
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8081707060:AAFhCq5hM8btuDu1i17FGiTQUAqYonk8YyE")
+API_ID = int(os.environ.get("API_ID", "16536417"))
+API_HASH = os.environ.get("API_HASH", "f6e58a549da642d7b765744a2f82c6d9")
+PICS = os.environ.get("PICS", "https://envs.sh/_bz.jpg, https://envs.sh/_b3.jpg, https://envs.sh/_bC.jpg").split()
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1562935405').split()]
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://trumbot:trumbot@cluster0.cfkaeno.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DB_NAME", "DSBardAI")
 RemoveBG_API = os.environ.get("RemoveBG_API", "")
 IBB_API = os.environ.get("IBB_API", "")
 FORCE_SUB = os.environ.get("FORCE_SUB", "")
 PORT = os.environ.get('PORT', '8080')          
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001752715418'))
 LOG_TEXT = """<i><u>👁️‍🗨️USER DETAILS</u>
 
 ○ ID : <code>{id}</code>
